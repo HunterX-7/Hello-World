b494f4e28f92b93ed152125008f3f1afef1f3a61
HunterX-7 <fraquin7@gmail.com>
2023-05-10 11:43:25 -0500

    Setup routes in the App component to call Greeting

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import { getGreeting } from './Greeting';
+import Greeting from './Greeting';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  const greet = useSelector((state) => state.greetings);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getGreeting());
-  }, [dispatch]);
   
   return (
-  <>
-    <h1 style={{textAlign: 'center'}}>Random Greeting Generator: </h1>
-    <h2 style={{textAlign: 'center', fontWeight: 'bold'}}>{greet}</h2>
-  </>
+    <Routes>
+      <Route path="/" element={<Greeting />} />
+    </Routes>
   );
 };
 
